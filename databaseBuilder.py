1af03ea56c7b8263e927a89d4de474a146b8ebe0
joeacanfora <joeaca04@gmail.com>
2015-04-30 06:21:39 +0000

    fixed typos

--- databaseBuilder.py
+++ databaseBuilder.py
@@ -11,20 +11,7 @@ project_id VARCHAR(50), project_name VARCHAR(255), project_url VARCHAR(255), sta
 goal VARCHAR(15), end_date DATE, author_name VARCHAR(255), location VARCHAR(255),
 main_video_link VARCHAR(255), category VARCHAR(255), currency VARCHAR(5), graded1 BOOL, grader1PID VARCHAR(25),
 firmid VARCHAR(50),
-videopressent BOOL, videoquaility1 TINYINT, soldlevel1 TINYINT, othcompreference1 TINYINT,
-othcompname1 VARCHAR(50), founderschool1 BOOL,
-founderschoolname1 VARCHAR(50), founderstartup1 BOOL,
-founderstartupname1 VARCHAR(50), prototypes1 BOOL,
-endorsements1 BOOL, endorsementname1 VARCHAR(50),
-music1 BOOL, animations1 BOOL, patent1 BOOL, rewardsMentioned1 BOOL,
-pitchFounder1 BOOL, pitchTechnology1 BOOL, pitchCustomer1 BOOL, videoLength1 INT,
-graded2 BOOL, grader2PID VARCHAR(25), videoquaility2 TINYINT, soldlevel2 TINYINT, othcompreference2 TINYINT,
-othcompname2 VARCHAR(50), founderschool2 BOOL,
-music2 BOOL, animations2 BOOL, patent2 BOOL, rewardsMentioned2 BOOL,
-pitchFounder2 BOOL, pitchTechnology2 BOOL, pitchCustomer2 BOOL,
-founderschoolname2 VARCHAR(50), founderstartup2 BOOL,
-founderstartupname2 VARCHAR(50), prototypes2 BOOL, videoLength2 INT,
-endorsements2 TINYINT, endorsementname2 VARCHAR(50),PRIMARY KEY(series_number), CONSTRAINT uc_ProjectID UNIQUE (project_id,project_name))""")
+videopresent BOOL,PRIMARY KEY(series_number), CONSTRAINT uc_ProjectID UNIQUE (project_id,project_name))""")
 
 #create rewards table
 c.execute("""CREATE TABLE rewards_table (reward_id MEDIUMINT NOT NULL AUTO_INCREMENT, series_number MEDIUMINT NOT NULL,
@@ -44,13 +31,12 @@ PRIMARY KEY(author_id), FOREIGN KEY (series_number) REFERENCES project_table(ser
 #create video_grades_table
 c.execute("""CREATE TABLE video_grades_table (grade_number MEDIUMINT NOT NULL AUTO_INCREMENT,
 project_id VARCHAR(50), series_number MEDIUMINT, graded BOOL, graderPID VARCHAR(25),
-firmid VARCHAR(50),
-videopressent BOOL, videoquaility TINYINT, soldlevel TINYINT, othcompreference TINYINT,
+firmid VARCHAR(50), videoquaility TINYINT, soldlevel TINYINT, othcompreference TINYINT,
 othcompname VARCHAR(50), founderschool BOOL,
 founderschoolname VARCHAR(50), founderstartup BOOL,
 founderstartupname VARCHAR(50), prototypes BOOL,
 endorsements BOOL, endorsementname VARCHAR(50),
-music BOOL, animations BOOL, paten1 BOOL, rewardsMentioned BOOL,
+music BOOL, animations BOOL, patent BOOL, rewardsMentioned BOOL,
 pitchFounder BOOL, pitchTechnology BOOL, pitchCustomer BOOL, videoLength INT,
 PRIMARY KEY (grade_number), FOREIGN KEY (series_number) REFERENCES project_table(series_number))""")
 
